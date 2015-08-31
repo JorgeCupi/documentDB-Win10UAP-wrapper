e2749bed23731b4f8d6e542fd4e59578d57bbbc7
Jorge Cupi <jorgecupi@hotmail.com>
2015-08-30 20:24:15 -0500

    First commit with basic CRUD operations on Documents within a collection

---
+++ DocumentDB/CRUD.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IoTChallenge.Universal.Core
+{
+    public static partial class documentDB
+    {
+        public static async Task<string> createDocument(string accountID, string dbID, string collectionID, string jsonFile)
+        {
+            string url = "https://{0}.documents.azure.com/dbs/{1}/colls/{2}/docs";
+            url = String.Format(url, accountID, dbID, collectionID);
+
+            StringContent body = GetStringContent(jsonFile);
+            HttpClient client = await GetClient("post", accountID, collectionID);
+
+            try
+            {
+                HttpResponseMessage result = await client.PostAsync(url, body);
+                return result.StatusCode.ToString() + " Documento creado";
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+        public static async Task<string> getDocuments(string accountID, string dbID, string collectionID)
+        {
+            string url = "https://{0}.documents.azure.com/dbs/{1}/colls/{2}/docs";
+            url = String.Format(url, accountID, dbID, collectionID);
+            HttpClient client = await GetClient("get", accountID, collectionID);
+
+            try
+            {
+                string result = await client.GetStringAsync(url);
+                return result;
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        public static async Task<string> getDocument(string accountID, string dbID, string collectionID, string documentID)
+        {
+            string url = "https://{0}.documents.azure.com/dbs/{1}/colls/{2}/docs/{3}";
+            url = String.Format(url, accountID, dbID, collectionID,documentID);
+            HttpClient client = await GetClient("get", accountID, documentID);
+
+            try
+            {
+                string result = await client.GetStringAsync(url);
+                return result;
+            }
+            catch (Exception ex) {return ex.Message;}
+        }
+
+        public static async Task<string> deleteDocument(string accountID, string dbID, string collectionID, string documentID)
+        {
+            string url = "https://{0}.documents.azure.com/dbs/{1}/colls/{2}/docs/{3}";
+            url = String.Format(url, accountID, dbID, collectionID, documentID);
+            HttpClient client = await GetClient("delete", accountID, documentID);
+
+            try
+            {
+                HttpResponseMessage result = await client.DeleteAsync(url);
+                return result.StatusCode.ToString() + " Documento eliminado";
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        public static async Task<string> updateDocument(string accountID, string dbID, string collectionID, string documentID, string jsonFile)
+        {
+            string url = "https://{0}.documents.azure.com/dbs/{1}/colls/{2}/docs/{3}";
+            url = String.Format(url, accountID, dbID, collectionID, documentID);
+
+            StringContent body = GetStringContent(jsonFile);
+            HttpClient client = await GetClient("put", accountID, documentID);
+
+            try
+            {
+                HttpResponseMessage result = await client.PutAsync(url, body);
+                return result.StatusCode.ToString() + " Documento actualizado";
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+    }
+}
---
+++ DocumentDB/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTChallenge.Universal.Core
+{
+    public static partial class documentDB
+    {
+        private static StringContent GetStringContent(string body)
+        {
+            StringContent stringContent = new StringContent(body);
+            stringContent.Headers.ContentLength = Encoding.UTF8.GetByteCount(body);
+            return stringContent;
+        }
+        private async static Task<HttpClient> GetClient(string verb, string accountID, string mainID)
+        {
+            HttpClient client = new HttpClient();
+            string date = DateTime.UtcNow.ToString("R", CultureInfo.InvariantCulture);
+            string authHeader = await Authenticate.GetAuthorizationHeader(verb, date, mainID);
+            client.DefaultRequestHeaders.Add("authorization", authHeader);
+            client.DefaultRequestHeaders.Add("x-ms-date", date);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Host", String.Format("{0}.documents.azure.com", accountID));
+            client.DefaultRequestHeaders.Add("x-ms-consistency-level", "Session");
+            client.DefaultRequestHeaders.Add("x-ms-version", "2015-04-08");
+            return client;
+        }
+    }
+}
